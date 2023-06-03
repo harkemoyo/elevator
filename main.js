12c0fc7128e804de383643aed8fb12fa17db58c2
harkemoyo <markndeche91@gmail.com>
2023-06-03 19:52:27 +0300

    added js

---
+++ main.js
@@ -0,0 +1,4 @@
+const moveUp = document.getElementById('moveUpBtn');
+const moveDown = document.getElementById('moveDownBtn');
+const selectFloor = document.getElementById('selectFloor');
+const goBtn = document.getElementById('goBtn');
