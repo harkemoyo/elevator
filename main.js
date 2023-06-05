939687ee24262a0896f982b0ea1af6dfbf77db5a
harkemoyo <markndeche91@gmail.com>
2023-06-05 11:54:40 +0300

    added js

--- main.js
+++ main.js
@@ -2,3 +2,14 @@ const moveUp = document.getElementById('moveUpBtn');
 const moveDown = document.getElementById('moveDownBtn');
 const selectFloor = document.getElementById('selectFloor');
 const goBtn = document.getElementById('goBtn');
+const floor = document.getElementById('floor');
+const lift = document.getElementById('lift');
+
+let toFloor = 0;
+let onMotion = false;
+let motion = null;
+
+document.addEventListener('DOMContentLoaded',() => {
+   onMotion = true;
+    motion = setInterval (AnimateUp, 50)
+});
